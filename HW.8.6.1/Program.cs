ae09404464ffbea3c18adcaff8fbb84c265c21a6
Андрей Срезневский <kiberman2015@mail.ru>
2023-08-10 20:59:59 +0300

    Добавьте файлы проекта.

---
+++ HW.8.6.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+//В заданиях использую диск H, так как на нем расположены все файлы по обучению
+try
+{
+    DirectoryInfo dirInfo = new DirectoryInfo(@"1111");
+    dirInfo.Delete(true); // Удаление со всем содержимым
+    Console.WriteLine("Каталог удален");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
